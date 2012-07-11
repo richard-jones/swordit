63ac3cb3d1ec57f898bc04b8943f32dee572aef0
Richard Jones <richard@cottagelabs.com>
2012-07-11 12:18:17 +0100

    add deposit of metadata as well as files

--- static/swordit.js
+++ static/swordit.js
@@ -36,10 +36,18 @@ function uploadClick(event, element)
 {
     event.preventDefault()
     
+    // first is to deposit the metadata
+    depositMetadata(function (uri) {
+        depositFile(uri)
+    })
+}
+
+function depositFile(em_uri)
+{
     var file = $("#file")[0].files[0]
     var filename = $("#file").val().substring("C:\\fakepath\\".length)
     $.ajax({
-        url: "http://sword:sword@localhost:8080/col-uri/08b48e2d-7f33-4388-80d4-34ac385d9569",
+        url: em_uri,
         type: 'POST',
         data: file,
         processData: false,
@@ -55,6 +63,43 @@ function uploadClick(event, element)
             201 : function(data) {
                     var deposit = $(data).find("link[rel='http://purl.org/net/sword/terms/originalDeposit']").attr('href')
                     setUploadedLink(deposit)
+                }
+        }
+    })
+}
+
+function depositMetadata(callback)
+{
+    // get the metadata to include in the deposit
+    var title = $("#deposit_title").val()
+    var abs = $("#deposit_abstract").val()
+    var author = $("#deposit_author").val()
+    
+    var xml = '<?xml version="1.0"?> \
+                <entry xmlns="http://www.w3.org/2005/Atom" \
+                        xmlns:dcterms="http://purl.org/dc/terms/"> \
+                    <title>' + title + '</title> \
+                    <id>urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a</id> \
+                    <author><name>' + author + '</name></author> \
+                    <summary type="text">' + abs + '</summary> \
+                </entry>'
+    $.ajax({
+        url: "http://sword:sword@localhost:8080/col-uri/08b48e2d-7f33-4388-80d4-34ac385d9569",
+        type: "POST",
+        data: xml,
+        dataType: 'xml',
+        processData: false,
+        headers : {
+            'Content-Type': 'application/atom+xml;type=entry',
+        },
+        beforeSend : function(xhr) {
+            xhr.setRequestHeader("Authorization", "Basic " + $.base64.encode("sword:sword"))
+        },
+        statusCode : {
+            201 : function(data) {
+                    // alert(data)
+                    var uri = $(data).find("link[rel='edit-media']").attr('href')
+                    callback(uri)
                 }
         }
     })
