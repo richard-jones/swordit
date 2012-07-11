99557406718d079bc3754150942010e60df4ff28
Richard Jones <richard@cottagelabs.com>
2012-07-11 11:26:31 +0100

    add some styling and better layout

--- static/swordit.js
+++ static/swordit.js
@@ -8,40 +8,48 @@ function depositClick(event, element)
     event.preventDefault()
     
     $(element).parent()
-        .html('<form><input type="file" id="file" name="depositfile">\
+        .html('<div style="border: 2px solid #7777ff; width: 250px; padding: 5px; background: #eeeeff">\
+                SWORD IT!<br><form><input type="file" id="file" name="depositfile"><br>\
                 <input id="uploadbutton" type="button" value="Upload"/>\
-                </form><a href="" class="close">close</a>')
+                <input class="close" type="button" value="Cancel">\
+                </form></div>')
     
     $(".close").click(function(event) {
         closeClick(event, this)
     })
     
     $("#uploadbutton").click(function(event) {
-        event.preventDefault()
-        
-        var file = $("#file")[0].files[0]
-        var filename = $("#file").val()
-        $.ajax({
-            url: "http://sword:sword@localhost:8080/col-uri/08b48e2d-7f33-4388-80d4-34ac385d9569",
-            type: 'POST',
-            data: file,
-            processData: false,
-            headers : {
-                'Content-Type': 'application/octet-stream',
-                'Packaging' : 'http://purl.org/net/sword/package/Binary',
-                'Content-Disposition' : 'attachment; filename=' + filename
-                },
-            beforeSend : function(xhr) {
-                xhr.setRequestHeader("Authorization", "Basic " + $.base64.encode("sword:sword"))
-                },
-            statusCode : {
-                201 : function(data) {
-                        var deposit = $(data).find("link[rel='http://purl.org/net/sword/terms/originalDeposit']").attr('href')
-                        setUploadedLink(deposit)
-                    }
-            }
-        })
+        uploadClick(event, this)
     });
+}
+
+function uploadClick(event, element)
+{
+    event.preventDefault()
+    
+    var file = $("#file")[0].files[0]
+    var filename = $("#file").val().substring("C:\\fakepath\\".length)
+    alert(filename)
+    $.ajax({
+        url: "http://sword:sword@localhost:8080/col-uri/08b48e2d-7f33-4388-80d4-34ac385d9569",
+        type: 'POST',
+        data: file,
+        processData: false,
+        headers : {
+            'Content-Type': 'application/octet-stream',
+            'Packaging' : 'http://purl.org/net/sword/package/Binary',
+            'Content-Disposition' : 'attachment; filename=' + filename
+        },
+        beforeSend : function(xhr) {
+            xhr.setRequestHeader("Authorization", "Basic " + $.base64.encode("sword:sword"))
+        },
+        statusCode : {
+            201 : function(data) {
+                    var deposit = $(data).find("link[rel='http://purl.org/net/sword/terms/originalDeposit']").attr('href')
+                    setUploadedLink(deposit)
+                }
+        }
+    })
 }
 
 function closeClick(event, element)
@@ -53,7 +61,7 @@ function closeClick(event, element)
 function setDepositLink()
 {
     $('#sworddepositor')
-        .html('<a href="" class="deposit">upload</a>')
+        .html('<form><input type="button" class="deposit" value="SWORD IT &gt;"></form>')
     
     $('.deposit').click(function(event) {
         depositClick(event, this)
@@ -63,5 +71,5 @@ function setDepositLink()
 function setUploadedLink(url)
 {
     $('#sworddepositor')
-        .html('<a href="' + url + '">download</a>')
+        .html('<a href="' + url + '">DOWNLOAD IT!</a>')
 }
